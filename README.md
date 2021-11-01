f2cab0a708e3ed8a55601fc567682075d2249915
theheroproject <58100691+theheroproject@users.noreply.github.com>
2021-11-02 02:26:25 +0900

    2021102

--- README.md
+++ README.md
@@ -3,3 +3,6 @@
 ## Dyanamo DB
 - Cards
   - id (PK)
+
+## AWS Lambda
+- 로그를 보기 위해서는 IAM 권한이 필요하다.
--- frontend/lambda/getCards.js
+++ frontend/lambda/getCards.js
@@ -1,9 +1,27 @@
+var AWS = require("aws-sdk");
+var documentClient = new AWS.DynamoDB({ apiVersion: "2012-08-10" });
+
+const tableName = "Cards";
+
 exports.handler = async (event) => {
-    console.log("Received: "+ JSON.stringify(event,null,2));
-    // TODO implement
-    const response = {
-        statusCode: 200,
-        body: JSON.stringify('Hello from Lambda!'),
+  console.log("Received: " + JSON.stringify(event, null, 2));
+  let response = "";
+  try {
+    var params = {
+      TableName: tableName,
     };
-    return response;
+
+    const cards = await documentClient.scan(params).promise();
+
+    response = {
+      statusCode: 200,
+      body: JSON.stringify(cards),
+    };
+  } catch (exception) {
+    response = {
+      statusCode: 500,
+      body: JSON.stringify({ "Message: ": exception }),
+    };
+  }
+  return response;
 };
